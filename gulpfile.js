afb05eff8ed521331e194ef35c0c211098280e8a
Michael Chavez <bigbassroller@gmail.com>
2014-11-18 23:34:54 -0800

    added stylus

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,5 @@
 var gulp = require('gulp')
-var concat = require('gulp-concat')
-
-gulp.task('js', function () {
-	gulp.src(['ng/module.js', 'ng/**/*.js'])
-		.pipe(concat('app.js'))
-		.pipe(gulp.dest('assets'))
+var fs = require('fs')
+fs.readdirSync(__dirname + '/gulp').forEach(function (task) {
+require('./gulp/' + task)
 })
