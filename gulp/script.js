e5e2c9366bdb3eb5b8543fd889c7719ae7eb5e1a
Michael Chavez <bigbassroller@gmail.com>
2014-11-19 22:16:46 -0800

    fixed bootstrap dropdown transitions by compliling transition.js before other bs.js

--- gulp/script.js
+++ gulp/script.js
@@ -5,7 +5,22 @@ var ngAnnotate = require('gulp-ng-annotate')
 var sourcemaps = require('gulp-sourcemaps')
 
 gulp.task('js', function () {
-  return gulp.src(['ng/module.js', 'ng/**/*.js', 'bower_components/bootstrap-stylus/js/*.js'])
+  return gulp.src([
+  	'ng/module.js', 
+  	'ng/**/*.js', 
+  	'bower_components/bootstrap-stylus/js/transition.js',
+  	'bower_components/bootstrap-stylus/js/affix.js',
+  	'bower_components/bootstrap-stylus/js/alert.js',
+  	'bower_components/bootstrap-stylus/js/button.js',
+  	'bower_components/bootstrap-stylus/js/carousel.js',
+  	'bower_components/bootstrap-stylus/js/collapse.js',
+  	'bower_components/bootstrap-stylus/js/dropdown.js',
+  	'bower_components/bootstrap-stylus/js/modal.js',
+  	'bower_components/bootstrap-stylus/js/popover.js',
+  	'bower_components/bootstrap-stylus/js/scrollspy.js',
+  	'bower_components/bootstrap-stylus/js/tab.js',
+  	'bower_components/bootstrap-stylus/js/tooltip.js'
+  	])
     .pipe(sourcemaps.init())
       .pipe(ngAnnotate())
       .pipe(concat('app.js'))
