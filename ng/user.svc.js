8761f3486cd823b32549d0aa266e8711dc231239
Michael Chavez <bigbassroller@gmail.com>
2014-12-01 00:47:38 -0800

    authenticating social posts

--- ng/user.svc.js
+++ ng/user.svc.js
@@ -1,38 +1,23 @@
 angular.module('app')
 .service('UserSvc', function ($http) {
-	var svc = this
-	svc.getUser = function () {
-		return $http.get('/api/users', {
-			headers: { 'X-Auth': this.token }
-		})
+  var svc = this
+  svc.getUser = function () {
+    return $http.get('/api/users')
 	}
-svc.login = function (username, password) {
-	return $http.post('/api/sessions', {
-		username: username, password: password
-	}).then(function (val) {
-		svc.token = val.data
-		return svc.getUser()
-	})
-}
+  svc.login = function (username, password) {
+    return $http.post('/api/sessions', {
+      username: username, password: password
+    }).then(function (val) {
+      svc.token = val.data
+      $http.defaults.headers.common['X-Auth'] = val.data
+      return svc.getUser()
+    })
+  }
+  // svc.register = function (username, password) {
+  //   return $http.post('/api/users', {
+  //     username: username, password: password
+  //   }).then(function () {
+  //     return svc.login(username, password)
+  //   })
+  // }
 })
-
-
-// angular.module('app')
-// .service('UserSvc', function ($http) {
-//   var svc = this
-//   svc.getUser = function () {
-//     return $http.get('/api/users')
-//     .then(function (response) {
-//       return response.data
-//     })
-//   }
-//   svc.login = function (username, password) {
-//     return $http.post('/api/sessions', {
-//       username: username, password: password
-//     }).then(function (response) {
-//       svc.token = response.data
-//       $http.defaults.headers.common['X-Auth'] = response.data
-//       return svc.getUser()
-//     })
-//   }
-// })
